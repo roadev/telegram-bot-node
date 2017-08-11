369e8b19308be631c684c307e4d767428119f8b6
Juan Roa <jdroa92@gmail.com>
2017-08-11 11:57:31 -0500

    Refactoring

--- herokuWebHook.js
+++ herokuWebHook.js
@@ -6,7 +6,6 @@ const options = {
   webHook: {
     port: process.env.PORT
   },
-  polling: true,
 };
 
 const url = process.env.APP_URL || '<URL>:443';
@@ -15,13 +14,11 @@ const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 bot.onText(/\/start/, (msg) => {
-
   bot.sendMessage(msg.chat.id, "Welcome", {
   "reply_markup": {
-    "keyboard": [["x2", "x3"], ["Keyboard"], ["I'm robot"]],
+    "keyboard": [["x2", "x3"]],
     }
   });
-
 });
 
 const double = (value) => value * 2;
@@ -29,35 +26,29 @@ const double = (value) => value * 2;
 const triple = (value) => value * 3;
 
 bot.on('message', (msg) => {
-  const x2 = "x2";
-  const x3 = "x3";
-  if (msg.text.toLowerCase().indexOf(x2) === 0) {
-    bot.sendMessage(msg.chat.id, "Enter a number to double").then(() => {
-      bot.once('message', answer => {
-        const res = answer.text;
-        const result = double(parseInt(res));
-        bot.sendMessage(msg.chat.id, result);
-        return result;
+
+  switch (msg.text.toLowerCase()) {
+    case 'x2':
+      bot.sendMessage(msg.chat.id, "Enter a number to double").then(() => {
+        bot.once('message', answer => {
+          const res = answer.text;
+          const result = double(parseInt(res));
+          bot.sendMessage(msg.chat.id, result);
+          return result;
+        });
       });
-    });
+      break;
+    case 'x3':
+      bot.sendMessage(msg.chat.id, "Enter a number to triple").then(() => {
+        bot.once('message', answer => {
+          const res = answer.text;
+          const result = triple(parseInt(res));
+          bot.sendMessage(msg.chat.id, result);
+          return result;
+        });
+      });
+    default:
+      bot.sendMessage(msg.chat.id, "Invalid command, please try again");
   }
 
-  if (msg.text.toLowerCase().indexOf(x3) === 0) {
-    bot.sendMessage(msg.chat.id, "Enter a number to triple").then(() => {
-      bot.once('message', answer => {
-        const res = answer.text;
-        const result = triple(parseInt(res));
-        bot.sendMessage(msg.chat.id, result);
-        return result;
-      });
-    });
-  }
-  const bye = "bye";
-  if (msg.text.toLowerCase().includes(bye)) {
-      bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-  }
-  const robot = "I'm robot";
-  if (msg.text.indexOf(robot) === 0) {
-      bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
-  }
 });
