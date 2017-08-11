36cdf8f5db1e09c0cd1c302c1335e1ff6da6a057
Juan Roa <jdroa92@gmail.com>
2017-08-11 15:14:29 -0500

    Adding BTC price feature

--- herokuWebHook.js
+++ herokuWebHook.js
@@ -6,49 +6,68 @@ const options = {
   webHook: {
     port: process.env.PORT
   },
+  polling: true,
 };
+
+// "keyboard": [["last BTC price": 1], ["lowest": 2, "highest": 3]],
 
 const url = process.env.APP_URL || '<URL>:443';
 const bot = new TelegramBot(TOKEN, options);
+const apiUrl = 'https://www.bitstamp.net/api/ticker/';
 
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 bot.onText(/\/start/, (msg) => {
+
   bot.sendMessage(msg.chat.id, "Welcome", {
   "reply_markup": {
-    "keyboard": [["x2", "x3"]],
+    "keyboard": [["last": 'last'], ["lowest": 'low', "highest": 'high']],
     }
   });
+
 });
 
-const double = (value) => value * 2;
-
-const triple = (value) => value * 3;
+const getData = async (option) => {
+  try {
+    const data = await fetch(apiUrl);
+    const parsedData = await data.json();
+    console.log(parsedData[option]);
+    return parsedData[option];
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 bot.on('message', (msg) => {
 
-  switch (msg.text.toLowerCase()) {
-    case 'x2':
-      bot.sendMessage(msg.chat.id, "Enter a number to double").then(() => {
-        bot.once('message', answer => {
-          const res = answer.text;
-          const result = double(parseInt(res));
-          bot.sendMessage(msg.chat.id, result);
-          return result;
-        });
-      });
-      break;
-    case 'x3':
-      bot.sendMessage(msg.chat.id, "Enter a number to triple").then(() => {
-        bot.once('message', answer => {
-          const res = answer.text;
-          const result = triple(parseInt(res));
-          bot.sendMessage(msg.chat.id, result);
-          return result;
-        });
-      });
-    default:
-      bot.sendMessage(msg.chat.id, "Invalid command, please try again");
-  }
+  const price = getData(msg.text.toLowerCase());
+  bot.sendMessage(msg.chat.id, `${msg.text.toLowerCase()}: ${price}`);
+
+  // switch (msg.text.toLowerCase()) {
+  //   case 'last':
+  //     const price = getData();
+  //     bot.sendMessage(msg.chat.id, `Last BTC price: ${price}`)
+  //     // .then(() => {
+  //     //   bot.once('message', answer => {
+  //     //     const res = answer.text;
+  //     //     const result = double(parseInt(res));
+  //     //     bot.sendMessage(msg.chat.id, result);
+  //     //     return result;
+  //     //   });
+  //     // });
+  //     break;
+  //   case 2:
+  //     bot.sendMessage(msg.chat.id, "Enter a number to triple")
+  //     // .then(() => {
+  //     //   bot.once('message', answer => {
+  //     //     const res = answer.text;
+  //     //     const result = triple(parseInt(res));
+  //     //     bot.sendMessage(msg.chat.id, result);
+  //     //     return result;
+  //     //   });
+  //     // });
+  //   default:
+  //     bot.sendMessage(msg.chat.id, "Invalid command, please try again");
+  // }
 
 });
